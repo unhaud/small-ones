a6f5429ca1f7d3c622410d4455ef9589407d8caa
unhaud <unhaud@gmail.com>
2025-10-28 22:07:34 +0300

    Color sheet button in Font scene
    -added a ColorChange button which opens a color sheet

--- FunnyFont/funny_font.gd
+++ FunnyFont/funny_font.gd
@@ -1,5 +1,24 @@
 extends Control
 
+@onready var label = $Label
+
+func _on_color_ShowColorPicker() -> void:
+	var color_picker = ColorPicker.new()
+	var popup = Popup.new()
+	popup.add_child(color_picker)
+	add_child(popup)
+	
+	color_picker.color_changed.connect(_on_color_changed)
+	popup.popup_hide.connect(popup.queue_free)
+	
+	# Получаем позицию кнопки на экране
+	var button_rect = $VBoxContainer/HBoxContainer/Color.get_global_rect()
+	# Открываем палитру под кнопкой
+	popup.popup(Rect2(button_rect.position + Vector2(0, button_rect.size.y), Vector2(300, 400)))
+
+func _on_color_changed(color):
+	label.add_theme_color_override("font_color", color)
+
 func _on_back_to_menu_button_up() -> void:
 	var BackToMenu = load("res://LevelSelector/LevelSelector.tscn")
 	get_tree().change_scene_to_packed(BackToMenu)
