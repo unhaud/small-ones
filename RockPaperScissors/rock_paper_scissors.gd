4189796315315beca77c518bad91c5598070e637
unhaud <unhaud@gmail.com>
2025-11-09 20:41:40 +0300

    Rock Paper Scissors: buttons signals
    added buttons signals

--- RockPaperScissors/rock_paper_scissors.gd
+++ RockPaperScissors/rock_paper_scissors.gd
@@ -1,1 +1,10 @@
 extends Control
+
+func _on_rock_button_up() -> void:
+	make_choice("rock")
+
+func _on_paper_button_up() -> void:
+	make_choice("paper")
+
+func _on_scissors_button_up() -> void:
+	make_choice("scissors")
