202505be97a6c3b4b9c10b22dafa496b1ab7fbc5
unhaud <unhaud@gmail.com>
2025-11-10 21:41:08 +0300

    Rock Paper Scissors: logic update
    added logic for Rock Paper Scissors scene

--- RockPaperScissors/rock_paper_scissors.gd
+++ RockPaperScissors/rock_paper_scissors.gd
@@ -1,5 +1,20 @@
 extends Control
 
+@onready var player_display = $ChoiceDisplay/HBoxContainer/PlayerDisplay
+@onready var computer_display = $ChoiceDisplay/HBoxContainer/ComputerDisplay
+@onready var result_label = $ResultLabel
+
+var choices = {
+	"rock": preload("res://RockPaperScissors/Art/rock.jpg"),
+	"scissors": preload("res://RockPaperScissors/Art/scissors.jpg"),
+	"paper": preload("res://RockPaperScissors/Art/paper.jpg"),
+	"unknown": preload("res://RockPaperScissors/Art/unknown.png")
+}
+
+func reset_displays():
+	player_display.texture = choices["unknown"]
+	computer_display.texture = choices["unknown"]
+
 func _on_rock_button_up() -> void:
 	make_choice("rock")
 
@@ -8,3 +23,21 @@ func _on_paper_button_up() -> void:
 
 func _on_scissors_button_up() -> void:
 	make_choice("scissors")
+
+func make_choice(player_choice):
+	player_display.texture = choices[player_choice]
+	
+	var computer_choice = ["rock", "scissors", "paper"][randi() % 3]
+	computer_display.texture = choices[computer_choice]
+	
+	determine_winner(player_choice, computer_choice)
+
+func determine_winner(player, computer):
+	if player == computer:
+		result_label.text = "Ничья!"
+	elif (player == "rock" and computer == "scissors") or \
+		 (player == "scissors" and computer == "paper") or \
+		 (player == "paper" and computer == "rock"):
+		result_label.text = "Вы победили!"
+	else:
+		result_label.text = "Компьютер победил!"
